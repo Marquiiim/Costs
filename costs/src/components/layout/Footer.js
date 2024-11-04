ebc56ce23beb1518827b72e1f06ab6b7873f371a
Marcos Gabriel <marcosindev@gmail.com>
2024-11-04 18:20:22 -0300

    Redirect footer informations

--- costs/src/components/layout/Footer.js
+++ costs/src/components/layout/Footer.js
@@ -1,20 +1,33 @@
+import { Link } from 'react-router-dom'
+
 import { FaWhatsapp, FaInstagram, FaLinkedin } from 'react-icons/fa'
 
 import styles from './Footer.module.css'
 
+
 function Footer() {
+
+    const RedirectToInstagram = () => {
+        window.open('https://www.instagram.com/mxrqiim/', '_blank', 'noopener,noreferrer')
+    }
+    const RedirectToWhatsapp = () => {
+        window.open('https://wa.me/85985720251', '_blank', 'noopener,noreferrer')
+    }
+    const RedirectToLinkedin = () => {
+        window.open('https://www.linkedin.com/in/marcos-gabriel-05287a322/', '_blank', 'noopener,noreferrer')
+    }
 
     return (
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
                 <li>
-                    <FaWhatsapp />
+                    <FaWhatsapp onClick={RedirectToWhatsapp} />
                 </li>
                 <li>
-                    <FaInstagram />
+                    <FaInstagram onClick={RedirectToInstagram} />
                 </li>
                 <li>
-                    <FaLinkedin />
+                    <FaLinkedin onClick={RedirectToLinkedin} />
                 </li>
             </ul>
             <p className={styles.copy_right}>
