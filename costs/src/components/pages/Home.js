b15d52fcfeb72dd8f303a22c69034e612a4f647c
Marcos Gabriel <marcosindev@gmail.com>
2024-11-04 17:56:58 -0300

    Home

--- costs/src/components/pages/Home.js
+++ costs/src/components/pages/Home.js
@@ -1,7 +1,18 @@
-function Home () {
+import styles from './Home.module.css'
+
+import savings from '../../img/savings.svg'
+
+import LinkButton from '../layout/LinkButton'
+
+function Home() {
 
     return (
-        <div>Home</div>
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <LinkButton to='/novoprojeto' text='Criar Projeto' />
+            <img src={savings} alt='Costs' />
+        </section>
     )
 }
 
