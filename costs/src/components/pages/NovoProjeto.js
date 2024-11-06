24911cb5ad022ba4be022cd7e8ba9fdf348eafac
Marcos Gabriel <marcosindev@gmail.com>
2024-11-06 17:17:02 -0300

    API LOCAL COM ERROR (EM ANALISE PARA CONSERTAR)

--- costs/src/components/pages/NovoProjeto.js
+++ costs/src/components/pages/NovoProjeto.js
@@ -1,14 +1,39 @@
+import { useNavigate } from 'react-router-dom'
+
 import styles from './NovoProjeto.module.css'
 
 import ProjetoForm from '../project/ProjetoForm'
 
-function NovoProjeto () {
+function NovoProjeto() {
+
+    const navigate = useNavigate()
+
+    function createPost(project) {
+
+        // Initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project)
+        })
+            .then(resp => resp.json())
+            .then((data) => {
+                console.log(data)
+                navigate('/projetos')
+            })
+            .catch(err => console.log(err))
+    }
 
     return (
         <div className={styles.novoprojeto_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços.</p>
-            <ProjetoForm btnText='Criar Projeto' />
+            <ProjetoForm btnText='Criar Projeto' handleSubmit={createPost} />
         </div>
     )
 }
