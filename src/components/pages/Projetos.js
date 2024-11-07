357181ec2593da8c93adfbab25715f9231a29f8b
Marcos Gabriel <marcosindev@gmail.com>
2024-11-07 18:40:34 -0300

    Revert "Loading dinâmico"
    This reverts commit db208b7b28b0b3fc8600a648eccadabd27610441.

--- src/components/pages/Projetos.js
+++ src/components/pages/Projetos.js
@@ -5,14 +5,12 @@ import Mensagem from '../layout/Mensagem'
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard'
-import Loading from '../layout/Loading'
 
 import styles from './Projetos.module.css'
 
 function Projetos() {
 
     const [projects, setProjects] = useState([])
-    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -21,20 +19,17 @@ function Projetos() {
     }
 
     useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:5000/projects', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                setProjects(data)
             })
-                .then(resp => resp.json())
-                .then(data => {
-                    setProjects(data)
-                    setRemoveLoading(true)
-                })
-                .catch((err) => console.log(err))
-        }, 800)
+            .catch((err) => console.log(err))
     }, [])
 
     return (
@@ -56,10 +51,6 @@ function Projetos() {
                             category={project.category ? project.category.name : 'Categoria não disponível'}
                             key={project.id} />
                     ))}
-                {!removeLoading && <Loading />}
-                {removeLoading && projects.length === 0 && (
-                    <p>Não há projetos cadastrados!</p>
-                )}
             </Container>
         </div>
     )
