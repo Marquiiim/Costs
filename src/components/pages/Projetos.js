535564e8fb889862ef2ac7176c957c3237abda99
Marcos Gabriel <marcosindev@gmail.com>
2024-11-07 18:57:03 -0300

    Update Projetos.js

--- src/components/pages/Projetos.js
+++ src/components/pages/Projetos.js
@@ -5,12 +5,14 @@ import Mensagem from '../layout/Mensagem'
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard'
+import Loading from '../layout/Loading'
 
 import styles from './Projetos.module.css'
 
 function Projetos() {
 
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -19,17 +21,20 @@ function Projetos() {
     }
 
     useEffect(() => {
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(resp => resp.json())
-            .then(data => {
-                setProjects(data)
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
             })
-            .catch((err) => console.log(err))
+                .then(resp => resp.json())
+                .then(data => {
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))
+        }, 800)
     }, [])
 
     return (
@@ -51,6 +56,10 @@ function Projetos() {
                             category={project.category ? project.category.name : 'Categoria não disponível'}
                             key={project.id} />
                     ))}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
