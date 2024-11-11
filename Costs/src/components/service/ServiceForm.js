10f7caa58940f70defdd33a619ac5f2653eed245
Marcos Gabriel <marcosindev@gmail.com>
2024-11-11 17:00:39 -0300

    Update ServiceForm.js

--- Costs/src/components/service/ServiceForm.js
+++ Costs/src/components/service/ServiceForm.js
@@ -1,10 +1,43 @@
+import { useState } from 'react'
+import Input from '../form/Input'
+import SubmitButton from '../form/Submit'
+
 import styles from './ServiceForm.module.css'
 
-function ServiceForm() {
+function ServiceForm({ handleSubmit, btnText, projectData }) {
+
+    function submit() {
+
+    }
+
+    function handleChange(e) {
+
+    }
 
     return (
-        <form>
-            <input />
+        <form onSubmit={submit} className={styles.form}>
+            <Input
+                type='text'
+                text='Nome do serviço'
+                name='name'
+                placeholder='Insira o nome do serviço'
+                handleOnChange={handleChange}
+            />
+            <Input
+                type='number'
+                text='Custo do serviço'
+                name='cost'
+                placeholder='Insira o valor do total'
+                handleOnChange={handleChange}
+            />
+            <Input
+                type='text'
+                text='Descrição do serviço'
+                name='description'
+                placeholder='Descreva o serviço'
+                handleOnChange={handleChange}
+            />
+            <SubmitButton text={textBtn} />
         </form>
     )
 }
