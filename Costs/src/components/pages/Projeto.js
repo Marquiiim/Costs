d1a386eac87f08c2967b1b3a58c1a0c6480c53cf
Marcos Gabriel <marcosindev@gmail.com>
2024-11-11 16:43:01 -0300

    Update Projeto.js

--- Costs/src/components/pages/Projeto.js
+++ Costs/src/components/pages/Projeto.js
@@ -14,6 +14,7 @@ function Projeto() {
 
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -35,6 +36,8 @@ function Projeto() {
     }, [id])
 
     function editPost(project) {
+        setMessage('')
+
         if (project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
             setType('error')
@@ -59,6 +62,10 @@ function Projeto() {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
 
     return (
@@ -92,6 +99,27 @@ function Projeto() {
                                 </div>
                             )}
                         </div>
+                        <div className={styles.service_form_container}>
+                            <h2>
+                                Adicione um serviço:
+                            </h2>
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                                {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                            </button>
+                            <div className={styles.project_info}>
+                                {showServiceForm &&
+                                    <div>
+                                        Formulário de serviço
+                                    </div>
+                                }
+                            </div>
+                        </div>
+                        <h2>Serviços</h2>
+                        <Container customClass="start">
+                            <p>
+                                Itens de serviços
+                            </p>
+                        </Container>
                     </Container>
                 </div>
             ) : (
